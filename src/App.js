cd722fc9345b5087f3f3891a8c2cdb9e94e8b5d5
malefor2 <malefor22@gmail.com>
2019-06-09 13:51:28 +0200

    ff

--- src/App.js
+++ src/App.js
@@ -2,37 +2,18 @@ import React from 'react';
 import './App.css';
 
 //TODO:
-//finish node cleanup
-
+//saving object array(localstorage)
 
 class Item extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      done: false,
-      render: true
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-  }
-  handleClick(){
-    this.setState({done: !this.state.done});
-  }
-  deleteItem(){
-    if(typeof this.props.id === 'number'){
-      this.props.remove(this.props.id);
-      this.setState({render: false});
-    }
-    else return;
-  }
   render(){
-    if(this.state.render){
+    if(this.props.render){
       return(
         <div className="listItem" >
-          <li onClick={this.handleClick}
-            className={((this.state.done) ? "done" : "notDone")}>
+          <li onClick={() => this.props.changeClass(this.props.id)}
+              className={((this.props.done) ? "done" : "notDone")}>
             {this.props.text}
-          </li><button onClick={this.deleteItem}>x</button>
+          </li>
+          <button onClick={() => this.props.remove(this.props.id)}>x</button>
         </div>
       );
     }else return null;
@@ -45,20 +26,29 @@ class List extends React.Component {
     this.state = {
       inText: '',
       inputI: 0,
-      items: []
+      items: [
+        {key: 10, text: "item1", done: true, render: true},
+        {key: 11, text: "item2", done: false, render: true},
+        {key: 21, text: "item4", done: false, render: true}
+      ]
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.removeListItem = this.removeListItem.bind(this);
-  };
+    this.changeCls = this.changeCls.bind(this);
+  }
   handleChange(event){
     this.setState({inText: event.target.value});
   }
   handleSubmit(event) {
     event.preventDefault();
     if(this.state.inText.length > 0){
-      this.state.items.push([this.state.inputI, this.state.inText]);
-      this.setState({inputI: this.state.inputI+1})
+      let o = {key: this.state.inputI,
+              text: this.state.inText,
+              done: false,
+              render: true};
+      this.state.items.push(o);
+      this.setState({inputI: this.state.inputI+1});
     }else{
       alert("too short");
       console.log(this.state.items);
@@ -67,20 +57,47 @@ class List extends React.Component {
     this.setState({inText: ''});
   }
   removeListItem(it){
-    //let n = this.state.items;
-
-    //n.splice(n.indexOf(it))
-    //this.setState({items: n});
-    //this.setState(this.state.items.splice(this.state.items[i],1));
+    let ar = this.state.items.slice();
+    let i = ar.indexOf(
+      ar.find(i => i.key === it)
+    );
+    ar.splice(i, 1);
+    this.setState({items: ar});
+  }
+  changeCls(it){
+    let item = this.state.items.find(i => i.key === it);
+    let ar = this.state.items.slice();
+    item.done = !item.done;
+    this.setState({items: ar});
   }
   renderItems(){
-    let items = this.state.items.map(
-      (t) => <Item remove={this.removeListItem}
-                   text={t[1]}
-                   key={t[0]}
-                   id={t[0]} />
-    );
+    let items = this.state.items.filter(item => !item.done);
     console.log(items);
+    items = items.map(
+      (t) => <Item remove={this.removeListItem}
+                   text={t.text}
+                   key={t.key}
+                   id={t.key}
+                   done={t.done}
+                   render={t.render}
+                   changeClass={this.changeCls}
+                   />
+    );
+    return(items);
+  }
+  renderDone(){
+    let items = this.state.items.filter(item => item.done);
+    console.log(items);
+    items = items.map(
+      (t) => <Item remove={this.removeListItem}
+                   text={t.text}
+                   key={t.key}
+                   id={t.key}
+                   done={t.done}
+                   render={t.render}
+                   changeClass={this.changeCls}
+                   />
+    );
     return(items);
   }
   render(){
@@ -90,8 +107,11 @@ class List extends React.Component {
           <input id="i" type="text" value={this.state.inText} onChange={this.handleChange}/>
           <input type="submit" value="Submit" onClick={this.handleSubmit}/>
         </form>
-        <ul id="notDone">
+        <ul id="notDone">TODO:
           {this.renderItems()}
+        </ul>
+        <ul id="done">DONE:
+          {this.renderDone()}
         </ul>
       </div>
     );
